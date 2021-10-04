32d5bc9a46cefa87743dac3813331285961e7c84
Feyza Topgul <feyzatopgul@gmail.com>
2021-10-03 17:41:51 -0700

    Added Calendar code

--- HelloCalendar/ViewController.swift
+++ HelloCalendar/ViewController.swift
@@ -7,9 +7,35 @@
 //
 
 import UIKit
+import EventKit
 
 class ViewController: UIViewController {
-
+    
+    @IBAction func addEventButton(_ sender: Any) {
+        let eventStore: EKEventStore = EKEventStore ()
+        eventStore.requestAccess(to: .event, completion: {(granted, error) in
+            if (granted) && (error == nil){
+                print("granted \(granted)")
+                print("error \(String(describing: error))")
+                let event: EKEvent = EKEvent(eventStore: eventStore)
+                event.title = "Hello Event"
+                event.startDate = Date ()
+                event.endDate = Date ()
+                event.notes = "This is a new event"
+                event.calendar = eventStore.defaultCalendarForNewEvents
+                do {
+                    try eventStore.save(event, span: .thisEvent)
+                }
+                catch let error as NSError {
+                    print("error \(error)")
+                }
+                print("Event saved")
+            }
+            else {
+                print("error \(String(describing: error))")
+            }
+        })
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
